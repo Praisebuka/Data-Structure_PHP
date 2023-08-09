b72c67d79f50e78591200bb672f3f05f50ac6a82
Praise Ebuka <praise.njoga@gmail.com>
2023-08-09 12:20:56 +0100

    working

--- linkedList/Http/singly.php
+++ linkedList/Http/singly.php
@@ -1,7 +1,33 @@
 <?php
+class LinkedList {
+    # create the head of the list that points to the head.
+    public $head; 
 
+    #then the constructor should initialize the head to null
+    public function __construct() {
+        $this->head = null;
+    }
+}
 
+class Node {
+    public $data;
+    public $next;
 
+    public function __construct($d) {
+        $this->data = $d;
+        $this->next = null;
+    }
+}
 
+function areIdentical($a, $b) {
+    while ($a != null && $b != null) {
+        if ($a->data != $b->data) {
+            return false;
+        }
+        $a = $a->next;
+        $b = $b->next;
+    }
+    return ($a == null && $b == null);
+}
 
 ?>
